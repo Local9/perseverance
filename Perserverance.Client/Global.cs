5475a9b0ef2fdb2717400e653610f542c4dbfc42
local9 <apjcookie1@hotmail.com>
2023-01-20 14:05:55 +0000

    feat(project): added Globals

---
+++ Perserverance.Client/Global.cs
@@ -0,0 +1,7 @@
+﻿global using CitizenFX.Core;
+global using Newtonsoft.Json;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using static CitizenFX.Core.Native.API;
---
+++ Perserverance.Server/Global.cs
@@ -0,0 +1,8 @@
+﻿global using CitizenFX.Core;
+global using Newtonsoft.Json;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Runtime.Serialization;
+global using System.Threading.Tasks;
+global using static CitizenFX.Core.Native.API;
