c0a059a00d4c031a07feb67676091e6d46fee9cd
local9 <apjcookie1@hotmail.com>
2023-02-17 01:31:52 +0000

    fix(tablet): fix keybind to open and close

--- Perseverance.Client/Managers/KeybindManager.cs
+++ Perseverance.Client/Managers/KeybindManager.cs
@@ -1,23 +1,25 @@
 ﻿namespace Perseverance.Client.Managers
 {
-    internal class KeybindManager : Manager<KeybindManager>
+    public class KeybindManager : Manager<KeybindManager>
     {
         public static bool IsTabletOpen { get; private set; }
+        const string COMMAND_OPEN_TABLET = "+openTablet";
 
         public override void Begin()
         {
-            RegisterKeyMapping("openTablet", "Open Tablet", "keyboard", "HOME");
-            RegisterCommand("openTablet", new Action<int, List<object>, string>(OnOpenTablet), false);
+            // HASH ~INPUT_6D37387D~
+            RegisterKeyMapping(COMMAND_OPEN_TABLET, "Open Tablet", "keyboard", "HOME");
+            RegisterCommand(COMMAND_OPEN_TABLET, new Action(OnOpenTablet), false);
 
-            NuiManager.AttachNuiHandler("hideUI", new EventCallback(metadata =>
+            RegisterNuiCallback("hideTabletUI", new Action<IDictionary<string, object>, CallbackDelegate>((body, result) =>
             {
                 IsTabletOpen = false;
                 NuiManager.SetFocus(false, false);
-                return new EventMessage();
+                result(new { success = true });
             }));
         }
 
-        private void OnOpenTablet(int source, List<object> args, string rawCommand)
+        private void OnOpenTablet()
         {
             IsTabletOpen = true;
             NuiManager.SetFocus(true, true);
