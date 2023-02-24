f7e82775002ea8da5207d5e7677642b023fe082f
local9 <apjcookie1@hotmail.com>
2023-02-24 22:04:28 +0000

    tweak(models): remove json attributes

--- Perseverance.Shared/Models/SnailyCAD/Citizen.cs
+++ Perseverance.Shared/Models/SnailyCAD/Citizen.cs
@@ -3,27 +3,13 @@
     public class Citizen
     {
         public string name { get; set; } // first name, not full name
-
         public string surname { get; set; }
-
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public object imageId { get; set; }
-
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public object imageBlurData { get; set; }
-
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string id { get; set; }
-
         public string userId { get; set; }
-
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string socialSecurityNumber { get; set; }
-
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public User user { get; set; }
-
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string fullname => $"{name} {surname}";
     }
 
--- Perseverance.Shared/Models/SnailyCAD/Registration.cs
+++ Perseverance.Shared/Models/SnailyCAD/Registration.cs
@@ -2,15 +2,9 @@
 {
     public class Registration
     {
-        [JsonProperty(Required = Required.Always)]
         public string username { get; set; }
-
-        [JsonProperty(Required = Required.Always)]
         public string password { get; set; }
-
-        [JsonProperty(Required = Required.Always)]
         public string confirmPassword { get; set; }
-
         public string registrationCode { get; set; }
         public object captchaResult { get; set; }
     }
