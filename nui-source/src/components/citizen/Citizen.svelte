17f0ce8e740e18f3ca37c517b51e19cad7c00d20
local9 <apjcookie1@hotmail.com>
2023-03-21 11:27:18 +0000

    feat: delete citizen confirmation modal

--- nui-source/src/components/citizen/Citizen.svelte
+++ nui-source/src/components/citizen/Citizen.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
   import Button from "@components/form/Button.svelte";
   import Icon from "@iconify/svelte";
+  import Modal from "@shared/Modal.svelte";
   import { deleteCitizen, setCitizen } from "@store/citizen";
   import type { ICitizen } from "../../@types/citizen";
   import CitizenForm from "./CitizenForm.svelte";
 
   export let citizen: ICitizen;
   export let showNameplate: boolean = true;
+
+  let showModal: boolean = false;
+  let showDeletionConfirmationModal: boolean = false;
 
   function onClickSetCitizen() {
     setCitizen(citizen);
@@ -15,12 +19,22 @@
   function onClickDeleteCitizen() {
     deleteCitizen(citizen);
   }
-
-  let showModal: boolean = false;
 </script>
 
 {#if showModal}
   <CitizenForm bind:showModal bind:citizen />
+{/if}
+
+{#if showDeletionConfirmationModal}
+  <Modal>
+    <div class="flex flex-col items-center justify-center">
+      <h1 class="text-2xl font-bold">Are you sure you want to delete this citizen?</h1>
+      <div class="flex flex-row items-center justify-center">
+        <Button class="w-32" on:click={() => (showDeletionConfirmationModal = false)}>Cancel</Button>
+        <Button class="w-32" variant="danger" on:click={onClickDeleteCitizen}>Delete</Button>
+      </div>
+    </div>
+  </Modal>
 {/if}
 
 <article>
@@ -43,7 +57,7 @@
   <div class="w-96 grid grid-cols-5" role="group">
     <Button class="text-ellipsis col-span-3 inline-block rounded-l h-10 w-64 overflow-hidden whitespace-nowrap" on:click={onClickSetCitizen}>{citizen.name} {citizen.surname}</Button>
     <Button class="inline-block rounded-none w-16" on:click={() => (showModal = true)}>Edit</Button>
-    <Button class="inline-block rounded-r w-16 text-center" variant="danger" on:click={onClickDeleteCitizen}>Delete</Button>
+    <Button class="inline-block rounded-r w-16 text-center" variant="danger" on:click={() => (showDeletionConfirmationModal = true)}>Delete</Button>
   </div>
 </article>
 
